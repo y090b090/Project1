4d0a620911b095ca1841c228600c8aa1f02a6e6d
Jongyun Ryu <jongyunyoo@naver.com>
2022-11-23 15:40:20 +0900

    button.c

---
+++ button.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <linux/input.h>
+#include <unistd.h>
+#include <fcntl.h> 
+#include <sys/ioctl.h> 
+#include <sys/msg.h>
+#include <pthread.h>
+#include "button.h"
+#define INPUT_DEVICE_LIST "/dev/input/event"
+#define PROBE_FILE "/proc/bus/input/devices" 
+#define HAVE_TO_FIND_1 "N: Name=\"ecube-button\"\n"
+#define HAVE_TO_FIND_2 "H: Handlers=kbd event"
+
+int buttonInit(void)
+{
+if (probeButtonPath(buttonPath) == 0)
+return 0;
+fd=open (buttonPath, O_RDONLY);
+msgID = msgget (MESSAGE_ID, IPC_CREAT|0666);
+pthread_create(&buttonTh_id, NULL, buttonThFunc, NULL);
+return 1;
+}
+
