d5a21b8bfd261fb76e1a9879a4a80665e9bcfaec
y090b090 <y090b090@gmail.com>
2022-11-25 23:27:37 +0900

    button 6

--- buttontest.c
+++ buttontest.c
@@ -7,11 +7,7 @@
 #include <sys/ioctl.h> // for ioctl
 #include <sys/msg.h>
 #include "button.h"
-// first read input device
-#define INPUT_DEVICE_LIST "/dev/input/event"
-//실제 디바이스 드라이버 노드파일: 뒤에 숫자가 붙음., ex)/dev/input/event5
-#define PROBE_FILE "/proc/bus/input/devices"
-//PPT에 제시된 "이 파일을 까보면 event? 의 숫자를 알수 있다"는 바로 그 파일
+
 int main(void)
 {
     printf("Buttontest Start");
