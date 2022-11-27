7b7342d60ce25ed5773063056aed82588ee7e164
kimjunho <12junho@naver.com>
2022-11-27 14:40:47 +0900

    ColorLed.h

---
+++ ColrLed.h
@@ -0,0 +1,13 @@
+
+#ifndef _COLOR_LED_DRV_H_
+#define _COLOR_LED_DRV_H_
+
+int pwmActiveAll(void);
+int pwmInactiveAll(void);
+int pwmSetDuty(int dutyCycle, int pwmIndex);
+int pwmSetPeriod(int Period, int pwmIndex);
+int pwmSetPercent(int percent, int ledColor);
+int pwmStartAll(void);
+int pwmLedInit(void);
+
+#endif //COLOR_LED_DRV_H_
