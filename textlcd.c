d3cf0b791ecd1245f785559482a6b7df070f2409
myeonghwanhong <audghks7245@naver.com>
2022-11-29 18:38:22 +0900

    text

--- textlcd.c
+++ textlcd.c
@@ -8,6 +8,11 @@ int lcdtextwrite(const char *str1,const char *str2,int lineFlag){
     memset(&stlcd,0,sizeof(stTextLCD));//구조체초기화
     linenum=lineFlag;
     printf("linenum:%d\n",linenum);
+    fd=open(TEXTLCD_DRIVER_NAME,O_RDWR);//opendriver
+    if(fd<0){
+    perror("driver(//dev//peritextlcd)openerror.\n");
+    return 1;
+    }
     if(linenum==0){
         stlcd.cmdData=CMD_DATA_WRITE_BOTH_LINE;
         len1 = strlen(str1);
@@ -15,11 +20,15 @@ int lcdtextwrite(const char *str1,const char *str2,int lineFlag){
         memcpy(stlcd.TextData[stlcd.cmdData - 1], str1, COLUMN_NUM);
         else
         memcpy(stlcd.TextData[stlcd.cmdData - 1], str1, len1);
+        stlcd.cmd=CMD_WRITE_STRING;
+        write(fd,&stlcd,sizeof(stTextLCD));
         len2 = strlen(str2);
         if ( len2 > COLUMN_NUM)
         memcpy(stlcd.TextData[stlcd.cmdData + COLUMN_NUM - 1], str2, COLUMN_NUM);
         else
         memcpy(stlcd.TextData[stlcd.cmdData + COLUMN_NUM - 1], str2, len2);
+        stlcd.cmd=CMD_WRITE_STRING;
+        write(fd,&stlcd,sizeof(stTextLCD));
     }
         
     else if(linenum==1){
@@ -29,6 +38,8 @@ int lcdtextwrite(const char *str1,const char *str2,int lineFlag){
         memcpy(stlcd.TextData[stlcd.cmdData - 1], str1, COLUMN_NUM);
         else
         memcpy(stlcd.TextData[stlcd.cmdData - 1], str1, len1);
+        stlcd.cmd=CMD_WRITE_STRING;
+        write(fd,&stlcd,sizeof(stTextLCD));
     }
     else if(linenum==2){
         stlcd.cmdData=CMD_DATA_WRITE_LINE_2;
@@ -37,19 +48,13 @@ int lcdtextwrite(const char *str1,const char *str2,int lineFlag){
         memcpy(stlcd.TextData[stlcd.cmdData - 1], str2, COLUMN_NUM);
         else
         memcpy(stlcd.TextData[stlcd.cmdData - 1], str2, len2);
+        stlcd.cmd=CMD_WRITE_STRING;
+        write(fd,&stlcd,sizeof(stTextLCD));
     }
     else{
         printf("lineFlag:%dwrong.range(0~2)\n",linenum);
         return 1;
     }
-
-    stlcd.cmd=CMD_WRITE_STRING;
-    fd=open(TEXTLCD_DRIVER_NAME,O_RDWR);//opendriver
-    if(fd<0){
-    perror("driver(//dev//peritextlcd)openerror.\n");
-    return 1;
-    }
-    write(fd,&stlcd,sizeof(stTextLCD));
     close(fd);
     return 0;
 
