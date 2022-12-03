47742849d83211c3f7cb2c0e793219d6c74e4cc3
kimjunho <12junho@naver.com>
2022-12-03 15:07:09 +0900

    temper

--- temperature.h
+++ temperature.h
@@ -1,13 +1,18 @@
 #ifndef _TEMPERATURE_H_
 #define _TEMPERATURE_H_
-
 char * spi_read_lm74(int file);
 
+#include <stdint.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 #include <fcntl.h>
-
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
 
 int spi_init(const char filename[40]){
     
