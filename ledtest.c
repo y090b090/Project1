c79e1c07e5811b7b76e9017bfeac327290fd5d83
kimjunho <12junho@naver.com>
2022-12-03 17:54:23 +0900

    commit

--- ledtest.c
+++ ledtest.c
@@ -5,11 +5,11 @@ int main(void)
 {
     int n=0;
     ledLibInit();
-    for(int n=0;n<8;n++){
         ledStatus();
-        ledOnOff(n,1);
+        ledOnOff(2,1);
+        ledOnOff(1,1);
+        ledOnOff(0,1);
         sleep(1);
-    }
     ledLibExit();
     return 0;
 }
