f397f6f99555562b6df691f57fabdaca25dac772
Jongyun Ryu <jongyunyoo@naver.com>
2022-12-05 20:11:16 +0900

    led minus

--- ledtest.c
+++ ledtest.c
@@ -5,11 +5,21 @@ int main(void)
 {
     int n=0;
     ledLibInit();
+    //ledOnOff(2,1);
+    ledallon();
+
+    sleep(1);
+    /*for(int i=i;i<8;i++){
         ledStatus();
-        ledOnOff(2,1);
-        ledOnOff(1,1);
-        ledOnOff(0,1);
+        ledOnOff(i,1);
+        usleep(1);
+    }*/
+    for(int i=0;i<8;i++){
+        ledminus();
         sleep(1);
+    }
+    
+
     ledLibExit();
     return 0;
 }
