e377d127899affbc7d26aa4dbdb55e3580fdb60b
myeonghwanhong <audghks7245@naver.com>
2022-12-05 15:29:15 +0900

    airplane

--- TFT-LCD/airplane/airplane.c
+++ TFT-LCD/airplane/airplane.c
@@ -4,3 +4,20 @@
 #include <sys/ioctl.h>
 #include <fcntl.h>
 #include "../libfbdev/libfbdev.h"
+
+int main(void){
+    int screen_width;
+    int screen_height;
+    int bits_per_pixel;
+    int line_length;
+    int cols = 0, rows = 0;
+	char *data;
+	//FrameBuffer init
+    if ( fb_init(&screen_width, &screen_height, &bits_per_pixel, &line_length) < 0 ){
+		printf ("FrameBuffer Init Failed\r\n");
+		return 0;
+	}
+    
+    fb_clear();
+    fb_close();
+}
